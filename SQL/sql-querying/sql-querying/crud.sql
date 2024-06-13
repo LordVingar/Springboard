936ac38aa81ed2102dae853167977019cc4f56cf
LordVingar <DGEcodes@gmail.com>
2024-06-12 21:23:10 -0500

    Completed SQL querying project

---
+++ SQL/sql-querying/sql-querying/crud.sql
@@ -0,0 +1,40 @@
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('chair', 44.00, FALSE);
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('stool', 25.99, TRUE);
+
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('table', 124.00, FALSE);
+
+
+SELECT * FROM products;
+
+
+SELECT name FROM products;
+
+
+SELECT name, price FROM products;
+
+
+INSERT INTO products (name, price, can_be_returned) VALUES ('lamp', 30.00, TRUE);
+
+
+SELECT * FROM products WHERE can_be_returned = TRUE;
+
+
+SELECT * FROM products WHERE price < 44.00;
+
+
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
+
+
+UPDATE products SET price = price - 20.00;
+
+
+DELETE FROM products WHERE price < 25.00;
+
+
+UPDATE products SET price = price + 20.00;
+
+
+UPDATE products SET can_be_returned = TRUE;
